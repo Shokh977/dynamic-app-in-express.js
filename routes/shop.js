0571f37d489602685a5e8dccbc4359e7bc94e58c
Shokhrukh <uphill7165@gmail.com>
2024-07-25 23:02:16 +0900

    added pug engine template to make the html dynamic. created 3 pug html syntax files.

--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
+const adminData = require('./admin')
 const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products
+ res.render('shop', {prods : products, docTitle: 'shop page'}) 
 });
 
 module.exports = router;
