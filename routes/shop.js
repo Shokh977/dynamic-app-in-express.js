c62402b67a070ef64f571c414adbe595b7d918af
Shokhrukh <uphill7165@gmail.com>
2024-07-26 22:02:41 +0900

    created layout for pug files.

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products
- res.render('shop', {prods : products, docTitle: 'shop page'}) 
+ res.render('shop', {prods : products, docTitle: 'shop page', path:'/'}) 
 });
 
 module.exports = router;
