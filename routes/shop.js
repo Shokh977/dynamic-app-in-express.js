160585bb238149f3bd4544035bdfae489501beaf
Shokhrukh <uphill7165@gmail.com>
2024-07-29 00:14:37 +0900

    added ejs template engines and used as a main engine. created (seperated layout) for ejs files

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,15 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products
- res.render('shop', {prods : products, docTitle: 'shop page', path:'/'}) 
+ res.render('shop', 
+  {
+   prods : products,
+   pageTitle: 'shop page',
+   path:'/',
+   hasProducts : products.length > 0,
+   activeShop:true,
+   productCSS : true,
+    }) 
 });
 
 module.exports = router;
