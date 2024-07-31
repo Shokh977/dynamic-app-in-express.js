a87e0a7d6c4e3b7a788e3fab9bb23b817d790596
Shokhrukh <uphill7165@gmail.com>
2024-07-31 22:37:09 +0900

    added model

--- controllers/products.js
+++ controllers/products.js
@@ -1,28 +1,29 @@
-const products = [];
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-    res.render("add-product", {
-      pageTitle: "add product",
-      path: "/admin/add-product",
-      formCSS: true,
-      productCSS: true,
-      activeAddProduct: true,
-    });
-  }
+  res.render("add-product", {
+    pageTitle: "add product",
+    path: "/admin/add-product",
+    formCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+};
 
-  exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
-    res.redirect("/");
-  }
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect("/");
+};
 
-  exports.getProducts = (req, res, next) => {
-   res.render('shop', 
-    {
-     prods : products,
-     pageTitle: 'shop page',
-     path:'/',
-     hasProducts : products.length > 0,
-     activeShop:true,
-     productCSS : true,
-      }) 
-  }
+exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
+  res.render("shop", {
+    prods: products,
+    pageTitle: "shop page",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
+};
